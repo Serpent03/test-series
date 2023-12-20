f1121a16499d1a72cc066a0d5ab4cd95320e4396
Serpent03 <dumbpilot03@gmail.com>
2023-12-20 09:16:15 +0530

    feat: bits_n_bytes

---
+++ 21.12.23/bits_n_bytes.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int bitAnd(int x, int y) {
+  // max ops: 8
+  // legal ops: ~, |
+  return ~(~x | ~y);
+}
+
+int bitOr(int x, int y) {
+  return ~(~x & ~y);
+}
+
+int bitXor(int x, int y) {
+  // max ops: 14
+  // legal ops: ~ &
+  return bitOr((~x & y), (x & ~y));
+}
+
+int sign(int x) {
+  // legal ops: ! ~ & ^ | + << >>
+  // max ops : 10
+  // return 1 if positive, 0 if zero, and -1 if negative
+
+  // implementing the !!x operator reduces the dimensionality
+  // of x from whatever number it was to either 0 or 1
+  return !!x | x >> 31;
+  // if x is 0, then x will be 0; as will be the bitshift operation to the right
+  // if x > 0; !!x == 1, x >> 31 is 0. OR operation results in 1
+  // if x < 0; !!x == 1, x >> 31 is 1
+}
+
+int getByte(int x, int n) {
+  return (x >> (n * 8)) & 0xFF;
+}
+
+int logicalShift(int x, int n) {
+  return x >> n & ~(0xF << (32 - n));
+}
+
+int conditional(int x, int y, int z) {
+  // return (x) ? y : z
+  // (!!x | y) - !!x
+  printf("%d :: -%d --%d\n", !x, (!!x & y), (!x & z));
+  return !!x & y & !!!x & z; 
+}
+
+int main() {
+  // printf("%d :: %d", bitAnd(4, 5), 4 & 5);
+  assert(bitAnd(4, 5) == 4 & 5);
+  assert(bitXor(4, 5) == 4 ^ 5);
+  assert(sign(-45) == -1);
+  assert(getByte(0x12345678, 1) == 0x56);
+  assert(logicalShift(0x87654321, 4) == 0x8765432);
+  assert(conditional(0, 4, 5) == 4);
+}
